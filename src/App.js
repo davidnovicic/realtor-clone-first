28cddef4bb2b84298600ba755e145533539517a1
Davidnovicic <davidnovicic3@gmail.com>
2024-10-20 20:35:51 +0200

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+
 function App() {
   return (
     <>
-      <h1> Hello World! </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
