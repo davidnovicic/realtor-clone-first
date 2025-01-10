4f5fd671e32967b2823129ad0c256c4a028ccb35
davidmacgenm <davidnovicic3@gmail.com>
2025-01-10 04:30:24 +0100

    Added share copy functionality and added Listing page

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,138 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { 
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper/modules";
+import "swiper/css/bundle";
+import { FaMapMarkerAlt, FaShare, FaBed, FaBath, FaParking, FaChair} from "react-icons/fa"
+
+
+
+export default function Listing() {
+           
+        const params = useParams();
+        const [listing, setListing] = useState(true);
+        const [loading, setLoading] = useState(true);
+        
+        const [shareLinkCopied, setShareLinkCopied] = useState(false)
+    
+    
+        
+        useEffect(() => {
+          async function fetchListing() {
+            const docRef = doc(db, "listings", params.listingId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+              setListing(docSnap.data());
+              setLoading(false);
+            }
+          }
+          fetchListing();
+        }, [params.listingId]);
+        if (loading) {
+          return <Spinner />;
+        }
+  
+    return (
+
+        <main>
+        <Swiper
+        modules={[Navigation, Pagination, Autoplay, EffectFade]}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true, type: "progressbar" }}
+        effect="fade"
+        autoplay={{ delay: 3000 }}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+        <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center" onClick={() => {
+                navigator.clipboard.writeText(window.location.href)
+                setShareLinkCopied(true)
+                setTimeout(() => {
+                    setShareLinkCopied(false)
+                }, 2000)
+        } }> 
+          <FaShare className="text-lg text-slate-500" />
+            </div>
+            {shareLinkCopied && (
+                <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2"> Link copied </p>
+            )}
+    
+            <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto rounded-lg shadow -lg bg-white p-4"> 
+                <div className="w-full h-[200px] lg-[400px]">
+                    <p className="text-2xl font-bold mb-3 text-blue-900"> 
+                        {listing.name} - $ {listing.offer ?
+                            listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") :
+                            listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}                             
+                        {listing.type === "rent" ? " / month" : ""}
+                    </p>
+                    <p className="flex items-center mt-6 mb-3 font-semibold ">
+                        <FaMapMarkerAlt className="text-green-700 mr-2" />
+                        {listing.address}
+                    </p>
+                    <div className="flex justify-start items-center space-x-4 w-[75%]"> 
+                        <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+                            {listing.type === "rent" ? "rent" : "sale"}
+                        </p>
+                        <p className="w-full"> 
+                            {listing.offer && (
+                                <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white font-semibold shadow-md text-center"> 
+                                    ${listing.regularPrice - listing.discountedPrice} discount
+                                </p>
+                            )}
+                        </p>
+                    </div>
+                    <p className="mt-3 mb-3">
+                        <span className="font-semibold mr-1">  
+                            Description -  
+                        </span>
+                        {listing.description}
+                    </p>
+                    <ul className="flex items-center space-x-2 sm:space-x-10"> 
+                        <li className="flex items-center whitespace-nowrap"> 
+                            <FaBed className="text-lg mr-1 "/>
+                            {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : "1 bedroom"}
+                        </li>
+                        <li className="flex items-center whitespace-nowrap"> 
+                            <FaBath className="text-lg mr-1" />
+                            {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : `1 Bathroom`}
+                        </li>
+                        <li className="flex items-center whitespace-nowrap"> 
+                            <FaParking className="text-lg mr-1" />
+                            {listing.parking ? `Parking spot ` : `No parking`}
+                        </li>
+                        <li className="flex items-center whitespace-nowrap"> 
+                            <FaChair className="text-lg mr-1" />
+                            {listing.furnished ? `Furnished` : `Not furnished`}
+                        </li>
+                    </ul>  
+                </div>
+                <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"> 
+                        
+                </div>
+            </div>
+
+            
+    </main>
+    )
+}
+ 
